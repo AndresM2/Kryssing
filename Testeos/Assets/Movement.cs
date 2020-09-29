bb79188d770315f49b3676ac6fb7de29fee169ba
xav1t0 <xmartinezjimenez@gmail.com>
2020-09-29 17:48:46 +0200

    Proyecto camaras

---
+++ Testeos/Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(0,0,-1*Time.deltaTime);
+        }
+    }
+}
